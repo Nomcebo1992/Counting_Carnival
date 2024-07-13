b27ecadc02e73938560756cf2e373613aee764eb
Nomcebo1992 <snembambo@gmail.com>
2024-07-13 14:20:52 +0200

    server.js

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// API Routes
+app.use('/api', apiRoutes);
+
+// Catch-all route to serve index.html for any other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
